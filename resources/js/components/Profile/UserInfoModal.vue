1251725047b739ab3375c0ca2b27332e34831852
reinisalpins <reinisalpins@gmail.com>
2023-04-14 21:23:54 +0300

    added profile store

---
+++ resources/js/components/Profile/UserInfoModal.vue
@@ -0,0 +1,20 @@
+<template>
+    <Button label="Labot profila informāciju" @click="visible = true"/>
+    <Dialog v-model:visible="visible" :draggable="false" modal header="Profila informācija"
+            :style="{ width: '50vw' }">
+                   <span class="p-float-label">
+
+                    </span>
+    </Dialog>
+</template>
+<script setup>
+import Dialog from 'primevue/dialog';
+import Button from 'primevue/button';
+import {ref} from "vue";
+
+
+
+const visible = ref(false);
+
+</script>
+<style lang="scss"></style>
